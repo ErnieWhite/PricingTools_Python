c7a02d67c7142500c439bd0d41d99b347c895aab
ErnieWhite <ernie.white.68@gmail.com>
2022-12-03 10:10:46 -0600

    Skeleton of formula convert complete

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # PricingTools
 Various tools I use to help me with my work
+
+## Formula Converter
+Allows the user to convert between different formula types
+Enter the current formula into the correct entry field and formula will be
+converted to all the other types.
+```
++----------------------------------+
+|              +----------+ +----+ |
+| Multiplier   |          | |Copy| |
+|              +----------+ +----+ |
+|              +----------+ +----+ |
+| Discount     |          | |Copy| |
+|              +----------+ +----+ |
+|              +----------+ +----+ |
+| Markup       |          | |Copy| |
+|              +----------+ +----+ |
+|              +----------+ +----+ |
+| Gross Profit |          | |Copy| |
+|              +----------+ +----+ |
++----------------------------------+
+```
+
+
--- formula_converter.py
+++ formula_converter.py
@@ -11,42 +11,51 @@ class ConverterFrame(ttk.Frame):
         self.multiplier_formula_label = ttk.Label(self, text='Multiplier')
         self.discount_formula_label = ttk.Label(self, text='Discount')
         self.markup_formula_label = ttk.Label(self, text='Markup')
-        self.grossprofit_formula_label = ttk.Label(self, text='Gross Profit')
+        self.gross_profit_formula_label = ttk.Label(self, text='Gross Profit')
 
         # create the entries
         self.multiplier_formula_entry = ttk.Entry(self)
         self.discount_formula_entry = ttk.Entry(self)
         self.markup_formula_entry = ttk.Entry(self)
-        self.grossprofit_formula_entry = ttk.Entry(self)
+        self.gross_profit_formula_entry = ttk.Entry(self)
 
         # create the copy buttons
-        self.multiplier_formula_button = ttk.Button(self)
-        self.discount_formula_button = ttk.Button(self)
-        self.markup_formula_button = ttk.Button(self)
-        self.grossprofit_formula_button = ttk.Button(self)
+        self.multiplier_formula_button = ttk.Button(self, text="Copy")
+        self.discount_formula_button = ttk.Button(self, text="Copy")
+        self.markup_formula_button = ttk.Button(self, text="Copy")
+        self.gross_profit_formula_button = ttk.Button(self, text="Copy")
+
+        self.place_widgets()
+
+        self.pack()
+
+    def place_widgets(self):
 
         # place the multiplier widgets
-        self.multiplier_formula_label.grid(column=0, row=0)
+        self.multiplier_formula_label.grid(column=0, row=0, sticky='w')
         self.multiplier_formula_entry.grid(column=1, row=0)
         self.multiplier_formula_button.grid(column=2, row=0)
 
         # place the discount widgets
-        self.discount_formula_label.grid(column=0, row=1)
+        self.discount_formula_label.grid(column=0, row=1, sticky='w')
         self.discount_formula_entry.grid(column=1, row=1)
         self.discount_formula_button.grid(column=2, row=1)
 
         # place the markup widget
-        self.markup_formula_label.grid(column=0, row=2)
+        self.markup_formula_label.grid(column=0, row=2, sticky='w')
         self.markup_formula_entry.grid(column=1, row=2)
         self.markup_formula_button.grid(column=2, row=2)
 
         # place the gross profit widgets
-        self.grossprofit_formula_label.grid(column=0, row=3)
-        self.grossprofit_formula_entry.grid(column=1, row=3)
-        self.grossprofit_formula_button.grid(column=2, row=3)
+        self.gross_profit_formula_label.grid(column=0, row=3, sticky='w')
+        self.gross_profit_formula_entry.grid(column=1, row=3)
+        self.gross_profit_formula_button.grid(column=2, row=3)
+
 
 if __name__ == '__main__':
     root = tk.Tk()
+
+    root.title("Formula Converter")
 
     frame = ConverterFrame(root)
 
